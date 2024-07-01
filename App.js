4c72119e10c78b893f8b0f6eaae9d128781524fc
adamanuszkiewicz <162536934+adamanuszkiewicz@users.noreply.github.com>
2024-06-30 19:18:35 -0700

    forgot to add these files

---
+++ App.js
@@ -0,0 +1,39 @@
+import Footer from './components/Footer';
+import React, { useState, useEffect } from 'react';
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from './data';
+import BookInfo from './pages/Bookinfo';
+import Cart from './pages/Cart';
+
+function App() {
+  const [cart, setCart] = useState([]);
+  
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect (() => {
+    console.log(cart)
+  }, [cart])
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route 
+          path="/books/:id" 
+          render={() => <BookInfo books={books} addToCart={addToCart} />} 
+        />
+        <Route path="/cart" render={() => <Cart books={books} cart={cart} />} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
